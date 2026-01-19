e50c9c1fd2eba724a6558824c1a32456116a0626
Kavya Chaturvedi <143157156+Kavychaturvedi5427@users.noreply.github.com>
2026-01-19 19:06:29 +0530

    Designed the Splash Activity.
    Designed and connected the Splash activity.

---
+++ app/src/main/java/com/kavya/unigo/MainSplash.java
@@ -0,0 +1,39 @@
+package com.kavya.unigo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
+
+public class MainSplash extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle SavedInstanceState){
+        super.onCreate(SavedInstanceState);
+        setContentView(R.layout.main_splash);
+
+        AppCompatImageView logo = findViewById(R.id.logo);
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.fade);
+        logo.post(()-> logo.startAnimation(animation));
+
+        ProgressBar progressBar = findViewById(R.id.Progress);
+        progressBar.setVisibility(View.VISIBLE);
+        Handler handler = new Handler();
+
+        handler.postDelayed(()->{
+            Intent intent = new Intent(getApplicationContext(), ChooseAuth.class);
+            startActivity(intent);
+
+            overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+            finish();
+        },2000);
+
+
+
+    }
+}
