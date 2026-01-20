3e06e696669125b416270779d4d7ac7a7897850c
Kavya Chaturvedi <143157156+Kavychaturvedi5427@users.noreply.github.com>
2026-01-21 01:42:49 +0530

    Minor Fixes.

--- app/src/main/java/com/kavya/unigo/MainSplash.java
+++ app/src/main/java/com/kavya/unigo/MainSplash.java
@@ -9,30 +9,30 @@ import android.view.animation.AnimationUtils;
 import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.AppCompatImageView;
 
 public class MainSplash extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle SavedInstanceState){
+    protected void onCreate(Bundle SavedInstanceState) {
         super.onCreate(SavedInstanceState);
         setContentView(R.layout.main_splash);
 
         AppCompatImageView logo = findViewById(R.id.logo);
-        Animation animation = AnimationUtils.loadAnimation(this,R.anim.fade);
-        logo.post(()-> logo.startAnimation(animation));
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade);
+        logo.post(() -> logo.startAnimation(animation));
 
         ProgressBar progressBar = findViewById(R.id.Progress);
         progressBar.setVisibility(View.VISIBLE);
         Handler handler = new Handler();
 
-        handler.postDelayed(()->{
+        handler.postDelayed(() -> {
             Intent intent = new Intent(getApplicationContext(), ChooseAuth.class);
             startActivity(intent);
 
-            overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
             finish();
-        },2000);
-
+        }, 2000);
 
 
     }
