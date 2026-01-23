984c37ab18762284069e595f7a90a356181f4eeb
Kavya Chaturvedi <143157156+Kavychaturvedi5427@users.noreply.github.com>
2026-01-23 13:28:26 +0530

    Designed Attendance BTM.
    Designed and implemented Attendance BTM to record daywise attendance of the student.

---
+++ app/src/main/java/com/kavya/unigo/AttendanceBtm.java
@@ -0,0 +1,153 @@
+package com.kavya.unigo;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AttendanceBtm extends BottomSheetDialogFragment {
+
+    TextInputEditText Attended, Total;
+    AppCompatButton save, cancel;
+    ProgressBar progressBar;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    String uid;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.attendance_btm, container, false);
+
+        if (auth.getCurrentUser() != null) {
+            uid = auth.getCurrentUser().getUid();
+        }
+
+        // binding view groups...
+        Attended = view.findViewById(R.id.attendedtxt);
+        Total = view.findViewById(R.id.totallectxt);
+        save = view.findViewById(R.id.savebtn);
+        cancel = view.findViewById(R.id.cancelbtn);
+        progressBar = view.findViewById(R.id.Progress);
+
+        save.setOnClickListener(v -> {
+            progressBar.setVisibility(View.VISIBLE);
+            String attendedStr = Attended.getText() != null
+                    ? Attended.getText().toString().trim() : "";
+            String totalStr = Total.getText() != null
+                    ? Total.getText().toString().trim() : "";
+
+            if (attendedStr.isEmpty() || totalStr.isEmpty()) {
+                Attended.setError("Required");
+                Total.setError("Required");
+                return;
+            }
+            int attendedToday = Integer.parseInt(attendedStr);
+            int totalToday = Integer.parseInt(totalStr);
+
+            if (attendedToday > totalToday || totalToday <= 0) {
+                Total.setError("Invalid values");
+                return;
+            }
+
+            // storing the attendance in the db...
+            db.collection("users").document(uid).get().addOnSuccessListener(documentSnapshot -> {
+                long oldAttended = 0, oldTotal = 0;
+                if (documentSnapshot.exists() && documentSnapshot.contains("attendance")) {
+                    Map<String, Object> attendance = (Map<String, Object>) documentSnapshot.get("attendance");
+                    if (attendance != null) {
+                        oldAttended = attendance.get("attended") != null ? (long) attendance.get("attended") : 0;
+
+                        oldTotal = attendance.get("total") != null ? (long) attendance.get("total") : 0;
+                    }
+                }
+
+                long newAttended = oldAttended + attendedToday;
+                long newtotal = oldTotal + totalToday;
+
+
+                Map<String, Object> attendanceMap = new HashMap<>();
+                attendanceMap.put("attended", newAttended);
+                attendanceMap.put("total", newtotal);
+                attendanceMap.put("updatedAt", FieldValue.serverTimestamp());
+
+                Map<String, Object> update = new HashMap<>();
+                update.put("attendance", attendanceMap);
+
+                db.collection("users").document(uid).set(update, SetOptions.merge()).addOnSuccessListener(unused -> {
+                            progressBar.setVisibility(View.GONE);
+                            Toast.makeText(getContext(), "Attendance stored.", Toast.LENGTH_SHORT).show();
+                            dismiss();
+                        })
+                        .addOnFailureListener(e -> {
+                            progressBar.setVisibility(View.GONE);
+                            Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                        });
+
+            }).addOnFailureListener(e -> {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            });
+
+            cancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dismiss();
+                }
+            });
+
+
+        });
+
+        return view;
+    }
+
+    // this gives access to the actual BTM container where mine is inflated...
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        BottomSheetDialog dialog = (BottomSheetDialog) super.onCreateDialog(savedInstanceState);
+
+        dialog.setOnShowListener(d -> {
+            BottomSheetDialog bottomSheetDialog = (BottomSheetDialog) d;
+            // this fetches the actual dialog which holds the UI
+            FrameLayout bottomSheet =
+                    bottomSheetDialog.findViewById(
+                            com.google.android.material.R.id.design_bottom_sheet
+                    );
+
+            // applying custom BTM to it
+            if (bottomSheet != null) {
+                bottomSheet.setBackground(
+                        ContextCompat.getDrawable(
+                                requireContext(),
+                                R.drawable.custom_btm
+                        )
+                );
+                bottomSheet.setClipToOutline(true);
+            }
+        });
+        return dialog;
+    }
+}
