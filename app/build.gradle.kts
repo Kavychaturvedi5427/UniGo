2e9d2b8e316d96aa87e36dbceb8ea5c109c18e96
Kavya Chaturvedi <143157156+Kavychaturvedi5427@users.noreply.github.com>
2026-03-06 21:05:41 +0530

    Update build.gradle.kts
    Added WorkManager dependency for notification and reformatted the build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,24 +42,43 @@ android {
 
 dependencies {
     implementation(libs.appcompat)
+
+    // lottie dependency
     implementation("com.airbnb.android:lottie:6.7.1")
+
+    // material ui dependency
     implementation("com.google.android.material:material:1.11.0")
+
+    // firebase dependency
     implementation(platform("com.google.firebase:firebase-bom:34.8.0"))
     implementation("com.google.firebase:firebase-analytics")
-    implementation ("com.google.firebase:firebase-auth")
-    implementation ("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+
+    // dependency used in MVVM arch for lifecycle awareness
     implementation("androidx.lifecycle:lifecycle-viewmodel:2.8.6")
     implementation("androidx.lifecycle:lifecycle-livedata:2.8.6")
-    implementation("com.google.firebase:firebase-storage")
-    implementation("com.github.bumptech.glide:glide:4.16.0")
+
     implementation(libs.vision.common)
+
+    // firebase ml toolkit dependency for image parsing
     implementation("com.google.mlkit:text-recognition:16.0.1")
+
+    // dependency to render image from the server
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+
+    // full image preview with zoom functionality
     implementation("com.github.chrisbanes:PhotoView:2.3.0")
 
+    // notification dependency
+    implementation("androidx.work:work-runtime:2.9.0")
+
+    // retrofit dependency
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
