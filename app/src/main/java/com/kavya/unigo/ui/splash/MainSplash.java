2eebd3982a90c2bed849dbfc4447541a2525d228
Kavya Chaturvedi <143157156+Kavychaturvedi5427@users.noreply.github.com>
2026-03-06 21:11:44 +0530

    Update MainSplash.java
    Called createChannel() for creating notification channel for the app and also setup a periodic system for sending notification for due assignment after every 12 hour.

--- app/src/main/java/com/kavya/unigo/ui/splash/MainSplash.java
+++ app/src/main/java/com/kavya/unigo/ui/splash/MainSplash.java
@@ -1,6 +1,9 @@
 package com.kavya.unigo.ui.splash;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -10,15 +13,43 @@ import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
+import androidx.work.ExistingPeriodicWorkPolicy;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
 
 import com.kavya.unigo.ui.auth.ChooseAuth;
 import com.kavya.unigo.R;
+import com.kavya.unigo.ui.features.workers.AssignmentReminder;
+import com.kavya.unigo.utils.NotificationHelper;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainSplash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle SavedInstanceState) {
         super.onCreate(SavedInstanceState);
         setContentView(R.layout.main_splash);
+
+        NotificationHelper.createChannel(this); // this will create the channel once and eveytime app restart or opens up again android will check whether channel for this app exists or not
+        // if no then it will create it otherwise not ...
+
+        // asks for the user permission to send notification...
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {    // this check whether the android version of the device is 13+ or not
+
+            if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                requestPermissions(
+                        new String[]{Manifest.permission.POST_NOTIFICATIONS},
+                        101
+                );
+            }
+        }
+
+        // worker will run every 12hr...
+        PeriodicWorkRequest assignmentReminder = new PeriodicWorkRequest.Builder(AssignmentReminder.class,12, TimeUnit.HOURS).build();
+
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork("assignment_reminder", ExistingPeriodicWorkPolicy.KEEP, assignmentReminder);
 
         AppCompatImageView logo = findViewById(R.id.logo);
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade);
